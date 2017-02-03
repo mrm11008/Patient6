ab2d6a7f705f80cbc81c32e82d9c8444343bf2ce
Maxwell Madonna <Max@n3-118-11.dhcp.drexel.edu>
2017-02-03 12:30:45 -0500

    added character controller, ray detection

---
+++ Patient Six/Assets/Scripts/characterController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class characterController : MonoBehaviour {
+
+	public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float translation = Input.GetAxis ("Vertical") * speed;
+		float straffe = Input.GetAxis ("Horizontal") * speed;
+		translation *= Time.deltaTime;
+		straffe += Time.deltaTime;
+
+		if (Input.GetAxis ("Vertical") > 0) {
+			transform.Translate (straffe / 100, 0, translation);
+		}
+		if (Input.GetAxis ("Horizontal") > 0) {
+			transform.Translate (straffe / 100, 0, translation);
+		}
+		if (Input.GetAxis ("Vertical") < 0) {
+			transform.Translate (straffe / 100, 0, translation);
+		}
+		if (Input.GetAxis ("Horizontal") < 0) {
+			transform.Translate (straffe / 100, 0, translation);
+		}
+//		transform.Translate (straffe, 0, translation);
+
+		if (Input.GetKeyDown ("escape")) {
+			Cursor.lockState = CursorLockMode.None;
+		}
+
+	}
+}
---
+++ Patient Six/Assets/Scripts/playerRayCasting.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerRayCasting : MonoBehaviour {
+
+	public float distanceToSee;
+	RaycastHit whatIHit;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawRay (this.transform.position, this.transform.forward * distanceToSee, Color.blue);
+
+		if (Physics.Raycast (this.transform.position, this.transform.forward, out whatIHit, distanceToSee)) {
+			Debug.Log ("I touched something" + whatIHit.collider.gameObject.name);
+		}
+	}
+}
